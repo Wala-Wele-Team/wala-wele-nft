6e79b3a74ecfe46471a46dee062543a6579f2c07
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-07-14 06:25:07 +0700

    Test Route 🚨

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link, Head } from "@inertiajs/inertia-react";
+
+export default function Homepage(props) {
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    );
+}
